20a3878d7fcd0bafc1c1efb0fba33c1bd92a44ae
Shaik Firdos <fidduraza9876@gmail.com>
2023-07-11 14:05:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// JavaScript code for closing the navbar when clicked outside
-       document.addEventListener("DOMContentLoaded", function() {
-           var navbarToggler = document.querySelector(".navbar-toggler");
-           var navbarCollapse = document.querySelector(".navbar-collapse");
+ // JavaScript code for closing the navbar when clicked outside or on the toggler
+  document.addEventListener("DOMContentLoaded", function() {
+    var navbarToggler = document.querySelector(".navbar-toggler");
+    var navbarCollapse = document.querySelector(".navbar-collapse");
 
-           document.addEventListener("click", function(event) {
-               var isClickInsideNavbar = navbarToggler.contains(event.target) || navbarCollapse.contains(event.target);
+    document.addEventListener("click", function(event) {
+      var targetElement = event.target;
+      var isClickInsideNavbar = navbarToggler.contains(targetElement) || navbarCollapse.contains(targetElement);
 
-               if (!isClickInsideNavbar) {
-                   // Close the navbar when clicked outside
-                   navbarCollapse.classList.remove("show");
-               }
-           });
+      if (!isClickInsideNavbar && navbarCollapse.classList.contains("show")) {
+        navbarToggler.click();
+      }
+    });
 
-           navbarToggler.addEventListener("click", function() {
-               // Toggle the visibility of the navbar menu
-               navbarCollapse.classList.toggle("show");
-           });
-       });
+    navbarToggler.addEventListener("click", function() {
+      // Toggle the visibility of the navbar menu
+      if (!navbarCollapse.classList.contains("show")) {
+        navbarCollapse.classList.add("show");
+      } else {
+        navbarCollapse.classList.remove("show");
+      }
+    });
+  });
